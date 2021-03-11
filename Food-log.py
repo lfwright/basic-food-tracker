fc4f0e4f1f2539773be3b28ae560f29a74a48e27
Louis Forbes Wright <lfwright@protonmail.com>
2021-03-11 18:23:19 +0000

    Added parser and rebuild argument. Finished database initialise and example data creation

--- Food-log.py
+++ Food-log.py
@@ -12,14 +12,24 @@
 
 ## create gui with pyQt
 
-import sqlite
+import sqlite3
 import os
+import argparse
+from shutil import rmtree
 
 #
 # define initalisation function to
 #  create directory in filesystem to store db
 #  create db / tables
 #
+
+def run_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rebuild", help = "rebuild the files and database", action="store_true")
+    args = parser.parse_args()
+    if args.rebuild:
+        rmtree('/tmp/food-log')
+
 
 def move_to_directory():
     try:
@@ -29,91 +39,120 @@ def move_to_directory():
         os.mkdir('/tmp/food-log')
         os.chdir('/tmp/food-log')
 
+def attempt_execution(cur, sql):
+    try:
+        cur.execute(sql)
+    except Exception as e:
+        print('error in sql: {0} \n {1}'.format(e, sql))
+
 def initalise_database():
     #find program files and create directory
     move_to_directory()
+    
+    #run inital sql statements
 
-    #run inital sql queries
     conn = sqlite3.connect("fdlg.db")
     cur = conn.cursor()
 
+    print('initalising database')
     try:
-        cur.execute('''CREATE TABLE item
-                    (item_id INTEGER PRIMARY KEY
-                    ,calories REAL
-                    ,protein REAL
-                    ,carbohydrates REAL
-                    ,fat REAL
-                    ,item_name TEXT NOT NULL
-                    ,type_id INTEGER)'''
-        )
+        init_sql = [
+            '''CREATE TABLE item
+            (item_id INTEGER PRIMARY KEY
+            ,calories REAL
+            ,protein REAL
+            ,carbohydrates REAL
+            ,fat REAL
+            ,item_name TEXT NOT NULL
+            ,type_id INTEGER)'''
+            ,
+            
+            '''CREATE TABLE meal
+            (meal_id INTEGER NOT NULL
+            ,item_id INTEGER NOT NULL
+            ,amount REAL
+            ,meal_name TEXT NOT NULL
+            ,PRIMARY KEY (meal_id, item_id)
+            ,FOREIGN KEY (item_id) REFERENCES item(item_id)
+            )'''
+            ,
         
-        cur.execute('''CREATE TABLE meal
-                   (meal_id INTEGER NOT NULL
-                   ,item_id INTEGER NOT NULL
-                   ,amount REAL
-                   ,meal_name TEXT NOT NULL
-                    PRIMARY KEY (meal_id, item_id)
-                    FOREIGN KEY (item_id) REFERENCES item(item_id)
-                   )'''
-        )
+            '''CREATE TABLE journal
+            (journal_id INTEGER PRIMARY KEY
+            ,date_id TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+            ,meal_id INTEGER NOT NULL
+            ,FOREIGN KEY (meal_id) REFERENCES meal(meal_id)
+            )'''
+            ,
+	    
+            '''CREATE TABLE type_key
+            (type_id INTEGER PRIMARY KEY
+            ,type_name TEXT NOT NULL)'''
+            ,
+	    
+            '''INSERT INTO type_key(type_id, type_name)
+            values(0, 'grams')'''
+            ,
+	    
+            '''INSERT INTO type_key(type_id, type_name)
+            values(1, 'single')'''
+            ,
+	
+            '''INSERT INTO type_key(type_id, type_name)
+            values(2, 'volume')'''
+        ]
 
-        cur.execute('''CREATE TABLE journal
-                   (journal_id INTEGER PRIMARY KEY
-                   ,date_id INTEGER NOT NULL
-                   ,meal_id INTEGER NOT NULL
-                    FOREIGN KEY (meal_id) REFERENCES meal(meal_id)
-                   )'''
-        )
-
-        cur.execute('''CREATE TABLE type_key
-                   (type_id INTEGER PRIMARY KEY
-                   ,type_name TEXT NOT NULL)'''
-        )
-
-        conn.commit()
-
-        cur.execute('''INSERT INTO type_key(type_id, type_name)
-                       (0, 'grams');
-
-                       INSERT INTO type_key(type_id, type_name)
-                       (1, 'single');
-
-                       INSERT INTO type_key(type_id, type_name)
-                       (2, 'volume');'''
-        )
+        for sql in init_sql:
+            attempt_execution(cur, sql)
+            print(sql[0:30])
+            
+    except Exception as e:
+        print('exception: {}'.format(e))
+    finally:
         conn.commit()
         conn.close()
 
 def create_example_data():
     conn = sqlite3.connect("fdlg.db")
     cur = conn.cursor()
-
+    
     cur.execute('''INSERT INTO item(calories, protein, carbohydrates, fat, item_name, type_id)
-                   (0.23, 0.029, 0.013, 0.008, 'spinach', 0);
-
-                   INSERT INTO item(calories, protein, carbohydrates, fat, item_name, type_id)
-                   (8.84, 0, 0, 1, 'olive oil', 0);
-
-                   INSERT INTO item(calories, protein, carbohydrates, fat, item_name, type_id)
-                   (0.19, 0, 0 , 0, 'red wine vinegar', 0);
-
-                   INSERT INTO meal(meal_id, item_id, amount, meal_name)
-                   (1, 1, 140, 'spinach salad');
-
-                   INSERT INTO meal(meal_id, item_id, amount, meal_name)
-                   (1, 2, 34, 'spinach salad');
-
-                   INSERT INTO meal(meal_id, item_id, amount, meal_name)
-                   (1, 3, 17, 'spinach salad');
-                   '''
+    values(0.23, 0.029, 0.013, 0.008, 'spinach', 0)'''
     )
-    conn.commit()
-    conn.close()
+    
+    cur.execute('''INSERT INTO item(calories, protein, carbohydrates, fat, item_name, type_id)
+    values(8.84, 0, 0, 1, 'olive oil', 0)'''
+    )
+    
+    cur.execute('''INSERT INTO item(calories, protein, carbohydrates, fat, item_name, type_id)
+    values(0.19, 0, 0 , 0, 'red wine vinegar', 0)'''
+    )
+    
+    cur.execute('''INSERT INTO meal(meal_id, item_id, amount, meal_name)
+    values(1, 1, 140, 'spinach salad')'''
+    )
+    
+    cur.execute('''INSERT INTO meal(meal_id, item_id, amount, meal_name)
+    values(1, 2, 34, 'spinach salad')'''
+    )
+    
+    cur.execute('''INSERT INTO meal(meal_id, item_id, amount, meal_name)
+    values(1, 3, 17, 'spinach salad')'''
+    )
+
+    cur.execute('''INSERT INTO journal(meal_id)
+    values(1)'''
+    )
 
     
+    conn.commit()
+    conn.close()
+    
+    
 if __name__ == "__main__":
+    run_parser()
+    
     move_to_directory()
-    if not os.access("fdlg.db", os.F_OK):
-#        initalise_database()
-#        create_example_data()
+    if True: #not os.access("fdlg.db", os.F_OK):
+        initalise_database()
+        create_example_data()
